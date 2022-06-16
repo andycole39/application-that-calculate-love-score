ea174966858fc8b67083f60e392363f7c6a40c3d
yakubu andrew <105059452+andycole39@users.noreply.github.com>
2022-06-16 20:26:18 +0100

    Add files via upload

---
+++ ogtech.js
@@ -0,0 +1,7 @@
+let yourName = prompt("what is the boy name?");
+let theirName = prompt("what is the girl name?");
+
+let loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) ;
+
+alert("The Love Score for " + yourName + " and " + theirName + " = "+ " is " + loveScore + "%");
